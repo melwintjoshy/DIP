bf5232a1548353de88c0db04f0f1aa01741325f4
mj465 <melwin465@gmail.com>
2025-01-28 23:56:27 +0530

    final commit

--- p6.py
+++ p6.py
@@ -10,16 +10,16 @@ img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 r_min = np.min(img)
 r_max = np.max(img)
 
-# Define the desired output range (0 to 255 for 8-bit imgs)
+#define the desired output range (0 to 255 for 8-bit imgs)
 L = 255
 
-# Apply contrast stretching
+#apply contrast stretching
 contrast_stretched = ((img - r_min) / (r_max - r_min)) * L
 
-# Convert the result to uint8 type
+#convert the result to uint8 type
 contrast_stretched = contrast_stretched.astype(np.uint8)
 
-# Display the result
+#display the result
 cv2.imshow('Original img', img)
 cv2.imshow('Contrast Stretched img', contrast_stretched)
 cv2.waitKey(0)
