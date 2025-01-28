6e3a5704e406e9f55e170b1f466272916cf9c4ff
mj465 <melwin465@gmail.com>
2025-01-28 23:31:52 +0530

    code modified

--- p5.py
+++ p5.py
@@ -9,6 +9,11 @@ img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
 
 # Calculate histogram
 hist = cv2.calcHist([img], [0], None, [256], [0, 256])
+# [img]: The input image.
+# [0]: The channel index (0 for grayscale, 0/1/2 for B/G/R in color images).
+# None: No mask is used.
+# [256]: Number of bins (intensity levels).
+# [0, 256]: Range of pixel values.
 
 # Plot the histogram
 plt.figure()
