344c74d28cba2b25b9e48102fee11612c856c9a4
Melwin T Joshy <95244119+melwintjoshy@users.noreply.github.com>
2025-01-29 12:47:36 +0530

    code modified

--- p10.py
+++ p10.py
@@ -1,3 +1,4 @@
+
 import cv2
 import numpy as np
 
@@ -7,13 +8,11 @@ image_path = r"/home/student/Desktop/melwin_pythonLab/DIP/messi.jpg"
 #read image
 img = cv2.imread(image_path) 
 
-results = {'original': img}
-
 kernels = [3, 5, 7]
 for k in kernels:
         # Create normalized box kernel
-        kernel = np.ones((k, k), np.float32) / (k * k)
-        results[f'box_kernel_{k}x{k}'] = cv2.filter2D(img, -1, kernel)
+        kernel = cv2.GaussianBlur(img, (k,k), sigmaX=0)
+        results[f'gaussian_kernel_{k}x{k}'] = cv2.filter2D(img, -1, kernel)
 
 for name, img in results.items():
         cv2.imshow(name, img)
